1a66fa01ae46623b4cf95448a7d71c62cdb39bc9
MXjz <597145896@qq.com>
2021-05-16 15:51:39 +0800

    polish some code

--- dtx/dtx-seata-demo/dtx-seata-demo-bank2/src/main/java/cn/itcast/seatademo/bank2/Bank2Server.java
+++ dtx/dtx-seata-demo/dtx-seata-demo-bank2/src/main/java/cn/itcast/seatademo/bank2/Bank2Server.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
-@EnableFeignClients(basePackages = {"cn.itcast.dtx.seatademo.bank2.spring"})
+//@EnableFeignClients(basePackages = {"cn.itcast.dtx.seatademo.bank2.spring"})
 public class Bank2Server {
 	
 	public static void main(String[] args) {
